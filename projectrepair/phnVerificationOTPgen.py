8e663371cc2205a196b74d6e0b8a89bb40e1e89d
starlord <palashsk2.sk@live.com>
2019-07-22 17:26:31 +0530

    otp gen and send

---
+++ projectrepair/phnVerificationOTPgen.py
@@ -0,0 +1,20 @@
+import pyotp
+import random
+import string
+def randomString(stringLength=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+totp=pyotp.TOTP(str(randomString()))
+print (totp.now())
+from twilio.rest import Client
+client = Client("ACeb71e9e2fbe4c14efa04c2e47fc6a82b", "5b65b294e374b358a73e15bb0e0730c6")
+client.messages.create(to="+918167597272", 
+                       from_="+14432956802", 
+                       body=totp.now())
+
+
+
+
+
