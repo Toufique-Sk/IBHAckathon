0a6af449cd642fa62f472939384e84e483afcbb9
starlord <palashsk2.sk@live.com>
2019-07-22 13:13:05 +0530

    ShopModel

---
+++ projectrepair/model.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from werkzeug.security import generate_password_hash, check_password_hash
+
+engine = create_engine("mysql+pymysql://starlord:qwerty@127.0.0.1/toufique?host=localhost?port=3306", echo=True)
+Base = declarative_base()
+
+from sqlalchemy import Column, Integer, String
+class Shop(Base):
+    __tablename__ = 'Shop'
+    id = Column(Integer, primary_key=True)
+    lati = Column(String)
+    longi = Column(String)
+    emailid = Column(String)
+    phnno=Column(Integer)
+    city=Column(String)
+    state=Column(String)
+    password=Column(String(128))  ## this must be hashed
+    rating=Column(Integer)
+
+    def __repr__(self):
+        return "<User(name='%s', fullname='%s', nickname='%s')>" % (self.name, self.fullname, self.nickname)
+
+
